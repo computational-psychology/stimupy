90f37f34bdce2cd4159566e02ea0a9fe16393906
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2020-04-10 16:03:15 +0200

    added some tests in testing_contrast_metrics.py, not passing

--- test_contrast_metrics.py
+++ test_contrast_metrics.py
@@ -1,12 +1,62 @@
 from src import contrast_metrics as cm
 import numpy as np
 
+# %% testing chunking
+# size of image
+s = (30, 30)
+
+# creating a matrix with random values, Gausian-distributed with s.d. = sigma 
+# the 'true' SD contrast is thus 0.1
+arr = np.ones(s) + np.random.normal(loc=0, scale=0.1, size=s)
+
+x = cm.SD(arr, mode="chunk", chunk_size=2)
+y = cm.SD(arr, mode="complete")
+z = cm.SD(arr, mode="unique")
+
+# the three methods of computing the metric should give results close to the true
+# value, and similar among each other
+print(x.round(3), y.round(3), z.round(3))
+assert(x==y)
+assert(y==z)
+# TODO: not the case for when chunk_size > 1
+
+# %% testing with known ground truths
+# TODO: maybe it would be a good idea to do some unittesting here with known ground truths
+arr = np.ones(s)
+x = cm.SD(arr, mode="chunk", chunk_size=5)
+y = cm.SD(arr, mode="complete")
+z = cm.SD(arr, mode="unique")
+assert(x==0.0)
+assert(y==0.0)
+assert(z==0.0)
+
+
+arr = np.vstack((np.ones(s), np.zeros(s)))
+x = cm.SD(arr, mode="chunk", chunk_size=5)
+y = cm.SD(arr, mode="complete")
+z = cm.SD(arr, mode="unique")
+assert(x==0.5)
+assert(y==0.5)
+assert(z==0.5)
+
+# TODO: RMS is not returning correct value
+arr = np.vstack((np.ones(s), np.zeros(s)))
+x = cm.RMS(arr, mode="chunk", chunk_size=1)
+y = cm.RMS(arr, mode="complete")
+z = cm.RMS(arr, mode="unique")
+assert(x==1.0)
+assert(y==1.0)
+assert(z==1.0)
+
+print(cm.SD(arr)/arr.mean())
+
+
 # %%
 arr = np.random.randint(1, 10, (10, 10))
 
-x = cm.SAM(arr, mode="chunk", chunk_size=2)
-y = cm.SAM(arr, mode="complete")
-z = cm.SAM(arr, mode="unique")
+x = cm.SD(arr, mode="chunk", chunk_size=2)
+y = cm.SD(arr, mode="complete")
+z = cm.SD(arr, mode="unique")
 print(x, y, z)
 
 # %%
