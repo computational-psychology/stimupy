12ab7e7d6f0158d75553be566a23d741928ade58
Joris Vincent <joris.vincent@gmail.com>
2021-04-08 13:49:23 +0200

    import all illusions in illusions init

--- stimuli/illusions/__init__.py
+++ stimuli/illusions/__init__.py
@@ -1,2 +1,13 @@
-from .brightness_illusions import *
-from .brightness_illusions2 import *
+from .benary_cross import benarys_cross
+from .bullseye import bullseye_illusion
+from .checkerboard_contrast_contrast import checkerboard_contrast_contrast_effect
+from .checkerboard_sbc import checkerboard_contrast
+from .cube import cube_illusion
+from .dungeon import dungeon_illusion
+from .grating_induction import grating_induction
+from .grating import grating_illusion
+from .hermann import hermann_grid
+from .rings import ring_pattern
+from .sbc import simultaneous_brightness_contrast, simultaneous_contrast
+from .todorovic import todorovic_illusion
+from .whites import *
