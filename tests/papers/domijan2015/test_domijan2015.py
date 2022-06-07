bde86be0ace5abb29abc2cb475db9b804639fa78
Joris Vincent <joris.vincent@gmail.com>
2022-06-07 18:00:01 +0200

    Regression test Domijan2015 against pickled output

--- tests/papers/domijan2015/test_domijan2015.py
+++ tests/papers/domijan2015/test_domijan2015.py
@@ -1,76 +1,20 @@
+import os.path
+import pickle
+
 import numpy as np
+import pytest
 import stimuli.papers.domijan2015
+from stimuli.papers.domijan2015 import __all__ as stimlist
 
-import ground_truth
-
-
-def test_dungeon():
-    original = ground_truth.get_dungeon()
-    test = stimuli.papers.domijan2015.dungeon()["img"]
-    assert np.array_equal(original, test)
+data_dir = os.path.dirname(__file__)
+picklefile = os.path.join(data_dir, "domijan2015.pickle")
+loaded = pickle.load(open(picklefile, "rb"))
 
 
-def test_cube():
-    original = ground_truth.get_cube()
-    test = stimuli.papers.domijan2015.cube()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_grating():
-    original = ground_truth.get_grating()
-    test = stimuli.papers.domijan2015.grating()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_ring():
-    original = ground_truth.get_ring()
-    test = stimuli.papers.domijan2015.rings()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_bullseye():
-    original = ground_truth.get_bullseye()
-    test = stimuli.papers.domijan2015.bullseye()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_sbc():
-    original = ground_truth.get_sbc()
-    test = stimuli.papers.domijan2015.simultaneous_brightness_contrast()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_white():
-    original = ground_truth.get_white()
-    test = stimuli.papers.domijan2015.white()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_benary():
-    original = ground_truth.get_benary()
-    test = stimuli.papers.domijan2015.benary()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_todorovic():
-    original = ground_truth.get_todorovic()
-    test = stimuli.papers.domijan2015.todorovic()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_checkerboard():
-    original = ground_truth.get_checkerboard()
-    test = stimuli.papers.domijan2015.checkerboard()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_checkerboard_extended():
-    original = ground_truth.get_checkerboard_extended()
-    test = stimuli.papers.domijan2015.checkerboard_extended()["img"]
-    assert np.array_equal(original, test)
-
-
-def test_checkerboard_contrast_contrast():
-    original = ground_truth.get_contrast_contrast()
-    test = stimuli.papers.domijan2015.checkerboard_contrast_contrast()["img"]
-    assert np.array_equal(original, test)
+@pytest.mark.parametrize("stim", stimlist)
+def test_stim(stim):
+    func = getattr(stimuli.papers.domijan2015, stim)
+    assert np.all(func()["img"] == loaded[stim]["img"]), "imgs are different"
+    assert np.all(
+        func()["mask"] == loaded[stim]["mask"]
+    ), "masks are different"
