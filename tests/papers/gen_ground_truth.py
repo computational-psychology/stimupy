260eeef97fb9798169b9a79b7987e9b6bdfda0a9
Joris Vincent <joris.vincent@gmail.com>
2022-06-16 17:07:13 +0200

    Reduce gen_ground_truth to single (flexible) function

--- tests/papers/gen_ground_truth.py
+++ tests/papers/gen_ground_truth.py
@@ -2,69 +2,48 @@ import json
 from hashlib import sha1
 
 import numpy as np
+import stimuli.papers
+
+papers = ["RHS2007", "domijan2015", "murray2020"]
 
 
-def ground_truth_RHS2007():
-    import stimuli.papers.RHS2007
-    from stimuli.papers.RHS2007 import __all__ as stimlist
+def gen_ground_truth(paper):
 
-    stims = {}
+    # Get a reference to the actual module, from the name-string
+    paper_module = getattr(stimuli.papers, paper)
+
+    # Extract the list of stimulus functions exported by that paper module
+    stimlist = getattr(paper_module, "__all__")
+
+    stims = {}  # save the stimulus-dicts in a larger dict, with name as key
     for stimname in stimlist:
-        print("Generating " + "RHS2007." + stimname)
-        func = getattr(stimuli.papers.RHS2007, stimname)
+        print(f"Generating {paper}.{stimname}")
+
+        # Get a reference to the actual function
+        func = getattr(paper_module, stimname)
         try:
+            # Generate the stimulus-dict
             stim = func()
-            stim["img"] = sha1(stim["img"]).hexdigest()
-            stim["mask"] = sha1(stim["mask"]).hexdigest()
-            stims[stimname] = stim
-        except NotImplementedError:
-            print("-- not implemented")
 
-    with open("RHS2007.json", "w", encoding="utf-8") as f:
-        json.dump(stims, f, ensure_ascii=False, indent=4)
-
-
-def ground_truth_domijan2015():
-    import stimuli.papers.domijan2015
-    from stimuli.papers.domijan2015 import __all__ as stimlist
-
-    stims = {}
-    for stimname in stimlist:
-        print("Generating " + "domijan2015." + stimname)
-        func = getattr(stimuli.papers.domijan2015, stimname)
-        try:
-            stim = func()
-            stim["img"] = sha1(stim["img"]).hexdigest()
-            stim["mask"] = sha1(stim["mask"]).hexdigest()
-            stims[stimname] = stim
-        except NotImplementedError:
-            print("-- not implemented")
-
-    with open("domijan2015.json", "w", encoding="utf-8") as f:
-        json.dump(stims, f, ensure_ascii=False, indent=4)
-
-
-def ground_truth_murray2020():
-    import stimuli.papers.murray2020
-    from stimuli.papers.murray2020 import __all__ as stimlist
-
-    stims = {}
-    for stimname in stimlist:
-        print("Generating " + "murray2020." + stimname)
-        func = getattr(stimuli.papers.murray2020, stimname)
-        try:
-            stim = func()
+            # Hash (SHA1) "img" and "mask", and save only the hex
             stim["img"] = sha1(np.ascontiguousarray(stim["img"])).hexdigest()
             stim["mask"] = sha1(np.ascontiguousarray(stim["mask"])).hexdigest()
+
+            # Accumulate
             stims[stimname] = stim
         except NotImplementedError:
+            # Skip stimuli that aren't implemented
             print("-- not implemented")
+            pass
 
-    with open("murray2020.json", "w", encoding="utf-8") as f:
-        json.dump(stims, f, ensure_ascii=False, indent=4)
+    return stims
 
 
 if __name__ == "__main__":
-    ground_truth_domijan2015()
-    ground_truth_murray2020()
-    ground_truth_RHS2007()
+    # Generate the ground_truth dict for each paper; save as .JSON
+    for paper in papers:
+        stims = gen_ground_truth(paper)
+
+        # Save the dictionary of stimulus-dicts as (pretty) JSON
+        with open(f"{paper}.json", "w", encoding="utf-8") as f:
+            json.dump(stims, f, ensure_ascii=False, indent=4)
