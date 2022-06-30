f4f6e490799b20345645e6e9ea0d7360f18f57a7
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2022-06-30 17:57:13 +0200

    adapted herman grid to repo-use

--- stimuli/illusions/hermann.py
+++ stimuli/illusions/hermann.py
@@ -1,19 +1,44 @@
 import numpy as np
+from stimuli.utils import degrees_to_pixels
 
 
-###################################
-#           Hermann Grid          #
-###################################
-def hermann_grid(n_grid=100, space=5):
-    # TODO: the parameters aren't analogous to the other stimuli
+def hermann_grid(
+        ppd=10,
+        grid_size=(10, 10),
+        element_size=(1.5, 1.5, 0.2),
+        vback=0.,
+        vgrid=1.
+        ):
+    """
+    Hermann grid
 
-    grid = np.zeros([n_grid, n_grid], dtype=np.float32)
-    grid[::space, :] = 1
-    grid[:, ::space] = 1
+    Parameters
+    ----------
+    ppd : int
+        pixels per degree (visual angle)
+    grid_shape : (float, float)
+        height and width of grid in degree visual angle
+    element_size : (float, float, float)
+        height, width and thickness of individual elements in degree visual angle
+    vback : float
+        value of background
+    vgrid : float
+        value of grid
 
-    img = grid
+    Returns
+    -------
+    A stimulus dictionary with the stimulus ['img'] and target mask ['mask']
+    """
+
+    grid_height, grid_width = degrees_to_pixels(grid_size, ppd)
+    element_height, element_width, element_thick = degrees_to_pixels(element_size, ppd)
+
+    img = np.ones([grid_height, grid_width], dtype=np.float32) * vback
+    for i in range(element_thick):
+        img[i::element_height, :] = vgrid
+        img[:, i::element_width] = vgrid
+
     mask = None  # TODO add this
-
     return {"img": img, "mask": mask}
 
 
