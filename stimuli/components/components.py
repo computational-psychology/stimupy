f62d8ab3d8ddbad835ce2debe7078bd2f18ef5cb
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2022-06-30 19:20:58 +0200

    added disc component

--- stimuli/components/components.py
+++ stimuli/components/components.py
@@ -1,5 +1,5 @@
 import numpy as np
-from stimuli.utils import degrees_to_pixels
+from stimuli.utils import degrees_to_pixels, resize_array
 
 
 def rectangle(
@@ -234,4 +234,48 @@ def checkerboard(
         img[i, j] = vcheck1 if i % 2 == j % 2 else vcheck2
 
     img = img.repeat(check_size_px, axis=0).repeat(check_size_px, axis=1)
+    return img
+
+
+def disc(
+        ppd=20,
+        radius=3,
+        vback=0.,
+        vdisc=1.,
+        ssf=5,
+        ):
+    """
+    Create a central disc
+
+    Parameters
+    ----------
+    ppd : int
+        pixels per degree (visual angle)
+    radius : float
+        radius of disc in degree visual angle
+    vback : float
+        value of background
+    vdisc : float
+        value of disc
+    ssf : int (optional)
+          the supersampling-factor used for anti-aliasing. Default is 5.
+
+    Returns
+    -------
+    A 2d-array with a disc
+    """
+    radius_px = degrees_to_pixels(radius, ppd) * ssf
+
+    # create stimulus at 5 times size to allow for supersampling antialiasing
+    img = np.ones([radius_px*2, radius_px*2]) * vback
+
+    # compute distance from center of array for every point, cap at 1.0
+    x = np.linspace(-img.shape[1] / 2.0, img.shape[1] / 2.0, img.shape[1])
+    y = np.linspace(-img.shape[0] / 2.0, img.shape[0] / 2.0, img.shape[0])
+    dist = np.sqrt(x[np.newaxis, :] ** 2 + y[:, np.newaxis] ** 2)
+    img[dist < radius_px] = vdisc
+
+    # downsample the stimulus by local averaging along rows and columns
+    sampler = resize_array(np.eye(img.shape[0] // ssf), (1, ssf))
+    img = np.dot(sampler, np.dot(img, sampler.T)) / ssf**2
     return img
