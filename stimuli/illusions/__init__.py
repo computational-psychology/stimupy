042dae131435e3ece8adbd2e890e8d8e8138af2a
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2022-06-30 17:55:49 +0200

    put checkerboards in one script and removed padding+doubling from functions

--- stimuli/illusions/__init__.py
+++ stimuli/illusions/__init__.py
@@ -1,15 +1,12 @@
 from .benary_cross import benarys_cross
 from .bullseye import bullseye_stimulus
-from .checkerboard_contrast_contrast import (
-    checkerboard_contrast_contrast_effect,
-)
-from .checkerboard_sbc import checkerboard_contrast
+from .checkerboards import *
 from .cornsweet import cornsweet
 from .cube import cube_illusion
 from .disc_and_ring import disc_and_ring
 from .dungeon import dungeon_illusion
-from .grating import grating_illusion
-from .grating_induction import grating_illusion
+from .grating import *
+from .grating_induction import *
 from .hermann import hermann_grid
 from .mondrians import corrugated_mondrians
 from .rings import ring_stimulus
