a886dae689b0f1933c99170b96b2c45d690e2647
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2022-08-02 18:12:42 +0200

    updated old, flexible todo-functions and added user-friendlier versions

--- stimuli/illusions/todorovic.py
+++ stimuli/illusions/todorovic.py
@@ -3,13 +3,12 @@ from stimuli.components import cross, rectangle
 from stimuli.utils import degrees_to_pixels, pad_img_to_shape
 
 
-def todorovic_in(
-    im_size=(10.0, 10.0),
+def todorovic_rectangle_generalized(
+    shape=10,
     ppd=10,
     target_size=(4.0, 4.0),
     target_pos=(3.0, 3.0),
-    covers_height=2.0,
-    covers_width=2.0,
+    covers_size=(2., 2.),
     covers_posx=(2.0, 6.0, 2.0, 6.0),
     covers_posy=(2.0, 6.0, 6.0, 2.0),
     vback=0.0,
@@ -17,22 +16,21 @@ def todorovic_in(
     vcovers=1.0,
 ):
     """
-    Todorovic's illusion with rectangular target and rectangles added
+    Todorovic's illusion with rectangular target and rectangular covers added with flexible
+    number of covers and flexible target and cover placement
 
     Parameters
     ----------
-    im_size : (float, float)
+    shape : float or (float, float)
         size of the stimulus in degrees of visual angle (height, width)
     ppd : int
         pixels per degree (visual angle)
-    target_size : (float, float)
+    target_size : float or (float, float)
         size of the target in degrees of visual angle (height, width)
-    target_pos : (float, float)
+    target_pos : float or (float, float)
         coordinates where to place the target
-    covers_height : float or tuple of floats
-        height of covers; if single float, all covers have the same height
-    covers_width : float or tuple of floats
-        width of covers; if single float, all covers have the same width
+    covers_size : float or (float, float)
+        size of the covers in degrees of visual angle (height, width)
     covers_posx : tuple of floats
         x coordinates of covers; as many covers as there are coordinates
     covers_posy : tuple of floats
@@ -49,34 +47,28 @@ def todorovic_in(
     A stimulus dictionary with the stimulus ['img'] and target mask ['mask']
     """
 
-    if isinstance(covers_height, (float, int)):
-        covers_height = [covers_height] * len(covers_posx)
-    if isinstance(covers_width, (float, int)):
-        covers_width = [covers_width] * len(covers_posy)
-
-    if any(
-        len(lst) != len(covers_height)
-        for lst in [covers_width, covers_posx, covers_posy]
-    ):
-        raise Exception(
-            "covers_height, covers_width, covers_posx and covers_posy need the"
-            " same length."
-        )
+    if isinstance(shape, (float, int)):
+        shape = (shape, shape)
+    if isinstance(covers_size, (float, int)):
+        covers_size = (covers_size, covers_size)
+    if len(covers_posx) != len(covers_posy):
+        raise ValueError("Need as many x- as y-coordinates")
 
     # Create image with square
-    img = rectangle(ppd, im_size, target_size, target_pos, vback, vtarget)
+    img = rectangle(ppd, shape, target_size, target_pos, vback, vtarget)
 
     # Add covers
-    covers_height = degrees_to_pixels(covers_height, ppd)
-    covers_width = degrees_to_pixels(covers_width, ppd)
-    covers_posx = degrees_to_pixels(covers_posx, ppd)
-    covers_posy = degrees_to_pixels(covers_posy, ppd)
+    cheight, cwidth = degrees_to_pixels(covers_size, ppd)
+    cposx = degrees_to_pixels(covers_posx, ppd)
+    cposy = degrees_to_pixels(covers_posy, ppd)
 
-    for i in range(len(covers_height)):
-        img[
-            covers_posy[i] : covers_posy[i] + covers_height[i],
-            covers_posx[i] : covers_posx[i] + covers_width[i],
-        ] = vcovers
+    if np.max(cposx) < np.min(cposx)+cwidth or np.max(cposy) < np.min(cposy)+cheight:
+        raise ValueError("Covers overlap")
+
+    for i in range(len(covers_posx)):
+        img[cposy[i]:cposy[i]+cheight, cposx[i]:cposx[i]+cwidth] = vcovers
+        if cposy[i]+cheight > shape[0]*ppd or cposx[i]+cwidth > shape[1]*ppd:
+            raise ValueError("Covers do not fully fit into stimulus")
 
     mask = np.copy(img)
     mask[mask == vback] = 0
@@ -85,13 +77,80 @@ def todorovic_in(
     return {"img": img, "mask": mask}
 
 
-def todorovic_out(
-    im_size=(12.0, 12.0),
+def todorovic_rectangle(
+    shape=(10, 10),
     ppd=10,
-    target_size=(4.0, 4.0, 4.0, 4.0),
-    target_thickness=2.0,
-    covers_height=2.0,
-    covers_width=2.0,
+    target_size=(4, 4),
+    covers_size=(3., 3.),
+    covers_offset=(2., 2.),
+    vback=0.0,
+    vtarget=0.5,
+    vcovers=1.0,
+):
+    """
+    Todorovic's illusion with rectangular target in the center and four rectangular covers added
+    symmetrically around target center
+
+    Parameters
+    ----------
+    shape : float or (float, float)
+        size of the stimulus in degrees of visual angle (height, width)
+    ppd : int
+        pixels per degree (visual angle)
+    target_size : float or (float, float)
+        size of the target in degrees of visual angle (height, width)
+    covers_size : float or (float, float)
+        size of covers in degrees of visual angle (height, width)
+    covers_offset : float or (float, float)
+        distance from cover center to target center in (y, x)
+    vback : float
+        value for background
+    vtarget : float
+        value for target
+    vcovers : float
+        value for covers
+
+    Returns
+    -------
+    A stimulus dictionary with the stimulus ['img'] and target mask ['mask']
+    """
+    if isinstance(shape, (float, int)):
+        shape = (shape, shape)
+    if isinstance(target_size, (float, int)):
+        target_size = (target_size, target_size)
+    if isinstance(covers_size, (float, int)):
+        covers_size = (covers_size, covers_size)
+    if isinstance(covers_offset, (float, int)):
+        covers_offset = (covers_offset, covers_offset)
+
+    # Calculate placement of target and covers for generalized function:
+    tpos = np.array(shape) / 2 - np.array(target_size) / 2
+    y1 = tpos[0] + target_size[0]/2 - covers_offset[0] - covers_size[0]/2
+    x1 = tpos[1] + target_size[1]/2 - covers_offset[0] - covers_size[1]/2
+    y2 = tpos[0] + target_size[0]/2 + covers_offset[0] - covers_size[0]/2
+    x2 = tpos[1] + target_size[1]/2 + covers_offset[0] - covers_size[1]/2
+
+    stim = todorovic_rectangle_generalized(
+        shape=shape,
+        ppd=ppd,
+        target_size=target_size,
+        target_pos=tpos,
+        covers_size=covers_size,
+        covers_posx=(x1, x2, x2, x1),
+        covers_posy=(y1, y2, y1, y2),
+        vback=vback,
+        vtarget=vtarget,
+        vcovers=vcovers,
+        )
+    return stim
+
+
+def todorovic_cross_generalized(
+    shape=(12.0, 12.0),
+    ppd=10,
+    target_arms_size=(4.0, 4.0, 4.0, 4.0),
+    target_thickness=2.,
+    covers_size=2.0,
     covers_posx=(3.0, 7.0, 3.0, 7.0),
     covers_posy=(3.0, 7.0, 7.0, 3.0),
     vback=0.0,
@@ -99,22 +158,21 @@ def todorovic_out(
     vcovers=1.0,
 ):
     """
-    Todorovic's illusion with cross target and rectangles added
+    Todorovic's illusion with cross target and rectangular covers added with flexible number of
+    covers and flexible cover placement
 
     Parameters
     ----------
-    im_size : (float, float)
+    shape : (float, float)
         size of the stimulus in degrees of visual angle (height, width)
     ppd : int
         pixels per degree (visual angle)
-    target_size : (float, float, float, float)
+    target_arms_size : (float, float, float)
         size of the target's arms in degrees visual angle in form (top, bottom, left, right)
     target_thickness : float
         thickness of target cross
-    covers_height : float or tuple of floats
-        height of covers; if single float, all covers have the same height
-    covers_width : float or tuple of floats
-        width of covers; if single float, all covers have the same width
+    covers_size : float or (float, float)
+        size of covers in degrees visual angle (height, width)
     covers_posx : tuple of floats
         x coordinates of covers; as many covers as there are coordinates
     covers_posy : tuple of floats
@@ -131,34 +189,26 @@ def todorovic_out(
     A stimulus dictionary with the stimulus ['img'] and target mask ['mask']
     """
 
-    if isinstance(covers_height, (float, int)):
-        covers_height = [covers_height] * len(covers_posx)
-    if isinstance(covers_width, (float, int)):
-        covers_width = [covers_width] * len(covers_posy)
+    if isinstance(shape, (float, int)):
+        shape = (shape, shape)
+    if isinstance(covers_size, (float, int)):
+        covers_size = (covers_size, covers_size)
+    if len(covers_posx) != len(covers_posy):
+        raise ValueError("Need as many x- as y-coordinates")
 
-    if any(
-        len(lst) != len(covers_height)
-        for lst in [covers_width, covers_posx, covers_posy]
-    ):
-        raise Exception(
-            "covers_height, covers_width, covers_posx and covers_posy need the"
-            " same length."
-        )
+    img = cross(ppd, target_arms_size, target_thickness, vback, vtarget)
+    if img.shape[0] > shape[0]*ppd or img.shape[1] > shape[1]*ppd:
+        raise ValueError("your cross does not fit in requested stimulus size")
+    img = pad_img_to_shape(img, np.array(shape) * ppd, val=vback)
 
-    img = cross(ppd, target_size, target_thickness, vback, vtarget)
-    img = pad_img_to_shape(img, np.array(im_size) * ppd, val=vback)
+    cheight, cwidth = degrees_to_pixels(covers_size, ppd)
+    cposx = degrees_to_pixels(covers_posx, ppd)
+    cposy = degrees_to_pixels(covers_posy, ppd)
 
-    covers_height = degrees_to_pixels(covers_height, ppd)
-    covers_width = degrees_to_pixels(covers_width, ppd)
-    covers_posx = degrees_to_pixels(covers_posx, ppd)
-    covers_posy = degrees_to_pixels(covers_posy, ppd)
-
-    # Add covers
-    for i in range(len(covers_height)):
-        img[
-            covers_posy[i] : covers_posy[i] + covers_height[i],
-            covers_posx[i] : covers_posx[i] + covers_width[i],
-        ] = vcovers
+    for i in range(len(covers_posx)):
+        img[cposy[i]:cposy[i]+cheight, cposx[i]:cposx[i]+cwidth] = vcovers
+        if cposy[i]+cheight > shape[0]*ppd or cposx[i]+cwidth > shape[1]*ppd:
+            raise ValueError("Covers do not fully fit into stimulus")
 
     mask = np.copy(img)
     mask[mask == vback] = 0
@@ -167,13 +217,82 @@ def todorovic_out(
     return {"img": img, "mask": mask}
 
 
+def todorovic_cross(
+    shape=(10, 10),
+    ppd=32,
+    target_arms_size=3.,
+    target_thickness=1.,
+    covers_size=3.2,
+    vback=1.0,
+    vtarget=0.5,
+    vcovers=0.0,
+):
+    """
+    Todorovic's illusion with cross target and four rectangular covers added at inner cross corners
+
+    Parameters
+    ----------
+    shape : float or (float, float)
+        size of the stimulus in degrees of visual angle (height, width)
+    ppd : int
+        pixels per degree (visual angle)
+    target_arms_size : float or (float, float)
+        size of target arms in degrees of visual angle (top/bottom, right/left)
+    covers_size : float or (float, float)
+        size of covers in degrees of visual angle (height, width)
+    vback : float
+        value for background
+    vtarget : float
+        value for target
+    vcovers : float
+        value for covers
+
+    Returns
+    -------
+    A stimulus dictionary with the stimulus ['img'] and target mask ['mask']
+    """
+    if isinstance(shape, (float, int)):
+        shape = (shape, shape)
+    if isinstance(target_arms_size, (float, int)):
+        target_arms_size = (target_arms_size, target_arms_size)
+    if isinstance(covers_size, (float, int)):
+        covers_size = (covers_size, covers_size)
+    if len(target_arms_size) != 2:
+        raise ValueError("target_arms_size should be single number of (float, float)")
+
+    # Calculate placement of target and covers for generalized function:
+    center = np.array(shape) / 2
+    y1 = center[0] - target_thickness/2 - covers_size[0]
+    x1 = center[1] - target_thickness/2 - covers_size[1]
+    y2 = center[0] + target_thickness/2 - 1/ppd
+    x2 = center[1] + target_thickness/2 - 1/ppd
+
+    arm_size = (target_arms_size[0], target_arms_size[0], target_arms_size[1], target_arms_size[1])
+    stim = todorovic_cross_generalized(
+        shape=shape,
+        ppd=ppd,
+        target_arms_size=arm_size,
+        target_thickness=target_thickness,
+        covers_size=covers_size,
+        covers_posx=(x1, x2, x2, x1),
+        covers_posy=(y1, y2, y1, y2),
+        vback=vback,
+        vtarget=vtarget,
+        vcovers=vcovers,
+        )
+
+    return stim
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     from stimuli.utils import plot_stimuli
 
     stims = {
-        "Todorovic (in)": todorovic_in(),
-        "Todorovic (out)": todorovic_out(),
+        "Todorovic rectangle": todorovic_rectangle(),
+        "Todorovic rectangle, flex": todorovic_rectangle_generalized(),
+        "Todorovic cross": todorovic_cross(),
+        "Todorovic cross, flex": todorovic_cross_generalized(),
     }
     plot_stimuli(stims)
     plt.show()
