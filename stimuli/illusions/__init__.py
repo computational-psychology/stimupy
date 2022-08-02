f9bd8d2d1d54e9d96d7549454e08654467ed2696
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2022-08-02 18:15:29 +0200

    updated init because of new modules aka moved functions

--- stimuli/illusions/__init__.py
+++ stimuli/illusions/__init__.py
@@ -1,6 +1,7 @@
 from .benary_cross import benarys_cross
 from .bullseye import bullseye_stimulus
 from .checkerboards import *
+from .circular import *
 from .cornsweet import cornsweet
 from .cube import cube_illusion
 from .dungeon import dungeon_illusion
@@ -11,4 +12,5 @@ from .mondrians import corrugated_mondrians
 from .rings import ring_stimulus
 from .sbc import *
 from .todorovic import *
+from .wedding_cake import *
 from .whites import *
