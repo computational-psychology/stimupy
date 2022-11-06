dd03ea07dab4779ed26a252b369aba881b1c5d33
Joris Vincent <joris.vincent@gmail.com>
2022-11-06 18:55:02 +0100

    Extract plotting to utils.plotting

--- stimuli/utils/utils.py
+++ stimuli/utils/utils.py
@@ -2,7 +2,7 @@
 Provides some functionality for creating and manipulating visual stimuli
 represented as numpy arrays.
 """
-import matplotlib.pyplot as plt
+
 import numpy as np
 
 
@@ -215,50 +215,6 @@ def get_annulus_mask(shape, center, inner_radius, outer_radius):
     mask = np.logical_xor(mask1, mask2)
 
     return mask
-
-
-
-def compare_plots(plots):
-    M = len(plots)
-    for i, (plot_name, plot) in enumerate(plots.items()):
-        plt.subplot(1, M, i + 1)
-        plt.title(plot_name)
-        plt.imshow(plot, cmap="gray")
-    plt.show()
-
-
-def plot_stim(stim, mask=False, stim_name="stim", ax=None):
-    if ax is None:
-        ax = plt.gca()
-
-    if not mask:
-        ax.imshow(stim["img"], cmap="gray", vmin=0.0, vmax=1.0)
-    else:
-        img, mask = stim["img"], stim["mask"]
-        img = np.dstack([img, img, img])
-
-        mask = np.insert(np.expand_dims(mask, 2), 1, 0, axis=2)
-        mask = np.insert(mask, 2, 0, axis=2)
-        final = mask + img
-        final /= np.max(final)
-        ax.imshow(final)
-
-    ax.set_title(label=stim_name)
-    return ax
-
-
-def plot_stimuli(stims, mask=False):
-    import math
-
-    # Plot each stimulus+mask
-    n_stim = math.ceil(math.sqrt(len(stims)))
-    F = plt.figure(figsize=(n_stim * 3, n_stim * 3))
-    for i, (stim_name, stim) in enumerate(stims.items()):
-        ax = F.add_subplot(n_stim, n_stim, i + 1)
-        plot_stim(stim, mask, stim_name=stim_name, ax=ax)
-
-    plt.tight_layout()
-    plt.show()
 
 
 ###################################
