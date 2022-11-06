bf7e67052d430bab427da70fd27e08c39dbc21f5
Joris Vincent <joris.vincent@gmail.com>
2022-11-06 18:53:58 +0100

    Move write_array_to_image to utils.export

--- stimuli/utils/utils.py
+++ stimuli/utils/utils.py
@@ -4,26 +4,6 @@ represented as numpy arrays.
 """
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
-
-
-def write_array_to_image(filename, arr):
-    """
-    Save a 2D numpy array as a grayscale image file.
-
-    Parameters
-    ----------
-    filename : str
-        full path to the file to be creaated.
-    arr : np.ndarray
-        2D numpy array
-        The data to be stored in the image. Values will be cropped to [0,255].
-    """
-    if Image:
-        imsize = arr.shape
-        im = Image.new("L", (imsize[1], imsize[0]))
-        im.putdata(arr.flatten())
-        im.save(filename)
 
 
 def degrees_to_pixels(degrees, ppd):
