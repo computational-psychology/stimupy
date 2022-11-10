d70b13f4cd92cd16f7f7504e382f5df001ee9168
Joris Vincent <joris.vincent@gmail.com>
2022-11-10 17:37:43 +0100

    Bugfix: don't auto-import utils.utils

--- stimuli/__init__.py
+++ stimuli/__init__.py
@@ -1,8 +1,9 @@
-from stimuli import components
-from stimuli import contrast_metrics
-from stimuli import illusions
-from stimuli import texture
-from stimuli import transparency
-from stimuli import papers
-from stimuli import noises
-from .utils import utils
+from stimuli import (
+    components,
+    contrast_metrics,
+    illusions,
+    noises,
+    papers,
+    texture,
+    transparency,
+)
