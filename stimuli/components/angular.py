a8ed843abc3ef4d2b78fccd7cbbbc1d8865913f5
Joris Vincent <joris.vincent@gmail.com>
2022-11-14 15:21:15 +0100

    component to mask a contiguous segment of angles

--- stimuli/components/angular.py
+++ stimuli/components/angular.py
@@ -34,3 +34,38 @@ def img_angles(visual_size=None, ppd=None, shape=None):
     img_angles %= 2 * np.pi
 
     return img_angles
+
+
+def mask_angle(
+    angles,
+    visual_size=None,
+    ppd=None,
+    shape=None,
+):
+    """Mask a contiguous set of angles in image
+
+    Parameters
+    ----------
+    angles : Sequence[float, float]
+        lower- and upper-limit of angles to mask, in degrees
+    visual_size : Sequence[Number, Number], Number, or None (default)
+        visual size [height, width] of image, in degrees
+    ppd : Sequence[Number, Number], Number, or None (default)
+        pixels per degree [vertical, horizontal]
+    shape : Sequence[Number, Number], Number, or None (default)
+        shape [height, width] of image, in pixels
+
+    Returns
+    -------
+    dict[str, Any]
+        dict with boolean mask (key: bool_mask) for pixels falling in given angle,
+        and additional params
+    """
+
+    image_angles = img_angles(visual_size=visual_size, ppd=ppd, shape=shape)
+
+    # Create mask
+    inner_angle, outer_angle = np.deg2rad(angles)
+    bool_mask = (image_angles > inner_angle) & (image_angles <= outer_angle)
+
+    return {"mask": bool_mask, "visual_size": visual_size, "ppd": ppd}
