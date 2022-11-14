4aabae15b7e828074711e70dfdc770de02397f86
Joris Vincent <joris.vincent@gmail.com>
2022-11-14 16:08:54 +0100

    Resolve angular grating params

--- stimuli/components/angular.py
+++ stimuli/components/angular.py
@@ -190,3 +190,93 @@ def segment_masks(
         mask += bool_mask["mask"] * (idx + 1)
 
     return {"mask": mask, "visual_size": visual_size, "ppd": ppd}
+
+
+def resolve_angular_params(
+    shape=None,
+    visual_size=None,
+    ppd=None,
+    frequency=None,
+    n_segments=None,
+    segment_width=None,
+):
+    """Resolve (if possible) spatial parameters for angular grating, i.e., set of segments
+
+    Angular grating (circle segments) component takes the regular resolution parameters
+    (shape, ppd, visual_size). In addition, there has to be an additional specification
+    of the number of segments, and their width. This can be done in two ways:
+    a segment_width (in degrees) and n_segments, and/or by specifying the spatial frequency
+    of a angular grating (in cycles per degree)
+
+    The total shape (in pixels) and visual size (in degrees) has to match the
+    specification of the segments and their widths.
+    Thus, not all 6 parameters have to be specified, as long as the both the resolution
+    and the distribution of segments can be resolved.
+
+    Note: all segments in a grating have the same width
+
+    Parameters
+    ----------
+    shape : Sequence[Number, Number], Number, or None (default)
+        shape [height, width] of image, in pixels
+    visual_size : Sequence[Number, Number], Number, or None (default)
+        visual size [height, width] of image, in degrees
+    ppd : Sequence[Number, Number], Number, or None (default)
+        pixels per degree [vertical, horizontal]
+    frequency : Number, or None (default)
+        spatial frequency of angular grating, in cycles per degree
+    n_segments : int, or None (default)
+        number of segments
+    segment_width : Number, or None (default)
+        width of a single segment, in degrees
+
+    Returns
+    -------
+    dict[str, Any]
+        dictionary with all six resolution & size parameters resolved.
+    """
+
+    # Resolve resolution
+    resolution.resolve(shape=shape, visual_size=visual_size, ppd=ppd)
+
+    # Try to resolve number and width(s) of segments
+    # segment_width = degrees_per_segment = 1 / segments_per_degree = 1 / (2*frequency)
+    if segment_width is not None:
+        segment_per_degree = 1 / segment_width
+        if frequency is not None and segment_per_degree != 2 * frequency:
+            raise ValueError(
+                f"segment_width {segment_width} and frequency {frequency} don't match"
+            )
+    elif frequency is not None:
+        segment_per_degree = 2 * frequency
+    else:  # both are None:
+        segment_per_degree = None
+
+    # Logic here is that segment_width expresses "degrees per segment",
+    # which we can invert to segments_per_degree, analogous to ppd:
+    # n_segments = segments_per_degree * n_degrees
+    # is analogous to
+    # pix = ppd * n_degrees
+    # Thus we can resolve the number and spacing of segments also as a resolution
+    try:
+        n_segments, _, segment_per_degree = resolution.resolve_1D(
+            length=n_segments, visual_angle=360, ppd=segment_per_degree
+        )
+        segment_width = 1 / segment_per_degree
+        frequency = segment_per_degree / 2
+    except Exception as e:
+        raise Exception("Could not resolve grating frequency, segment_width, n_segments") from e
+
+    # Determine angles
+    angular_widths = itertools.repeat(segment_width, n_segments)
+    angles = [0] + [*itertools.accumulate(angular_widths)]
+
+    return {
+        "shape": shape,
+        "visual_size": visual_size,
+        "ppd": ppd,
+        "frequency": frequency,
+        "segment_width": segment_width,
+        "n_segments": n_segments,
+        "angles": angles,
+    }
