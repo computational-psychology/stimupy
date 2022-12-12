ea0f1df867ecd104eba9d32933a0c0c5650d23ae
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2022-12-12 14:38:23 +0100

    reorganized dungeon and added resolving

--- stimuli/illusions/dungeon.py
+++ stimuli/illusions/dungeon.py
@@ -1,16 +1,19 @@
 import numpy as np
-from stimuli.utils import degrees_to_pixels
+from stimuli.utils import resolution, pad_to_shape
 
 
 __all__ = [
     "dungeon_illusion",
 ]
 
+
 def dungeon_illusion(
-    ppd=10,
-    n_cells=5,
+    shape=None,
+    visual_size=None,
+    ppd=None,
+    cell_size=None,
+    n_cells=None,
     target_radius=1,
-    cell_size=1.0,
     intensity_background=0.0,
     intensity_grid=1.0,
     intensity_target=0.5,
@@ -20,14 +23,18 @@ def dungeon_illusion(
 
     Parameters
     ----------
-    ppd : int
-        pixels per degree (visual angle)
-    n_cells : int
+    shape : Sequence[Number, Number], Number, or None (default)
+        shape [height, width] of grating, in pixels
+    visual_size : Sequence[Number, Number], Number, or None (default)
+        visual size [height, width] of grating, in degrees
+    ppd : Sequence[Number, Number], Number, or None (default)
+        pixels per degree [vertical, horizontal]
+    cell_size : Sequence[Number, Number], Number, or None (default)
+        size of individual cell (height, width)
+    n_cells : Sequence[Number, Number], Number, or None (default)
         the number of square cells (not counting background) per dimension
     target_radius : int
         the "Manhattan radius" of the diamond target in # cells
-    cell_size : float
-        size per cell in degrees visual angle
     intensity_background : float
         intensity value for background
     intensity_grid : float
@@ -39,15 +46,29 @@ def dungeon_illusion(
     -------
     A stimulus dictionary with the stimulus ['img'] and target mask ['mask']
     """
-    cell_size_px = degrees_to_pixels(cell_size, ppd)
+    
+    if isinstance(visual_size, (float, int)) or (visual_size is None):
+        visual_size = (visual_size, visual_size)
+    if isinstance(cell_size, (float, int)) or (cell_size is None):
+        cell_size = (cell_size, cell_size)
+    if isinstance(n_cells, (float, int)) or (n_cells is None):
+        n_cells = (n_cells, n_cells)
+
+    params = resolve_dungeon_params(None, visual_size, ppd, n_cells, cell_size)
+    n_cells = (int(params["n_cells"][0]), int(params["n_cells"][1])) 
+
+    height, width = params["shape"].height, params["shape"].width
+    cheight = height / (n_cells[0] * 2 - 1)
+    cwidth = width / (n_cells[1] * 2 - 1)
 
     # create 2D array of grid
-    arr = np.ones((n_cells * 2 - 1, n_cells * 2 - 1)) * intensity_grid
-    mask_arr = np.zeros((n_cells * 2 - 1, n_cells * 2 - 1))
+    arr = np.ones((n_cells[0] * 2 - 1, n_cells[1] * 2 - 1)) * intensity_grid
+    mask_arr = np.zeros((n_cells[0] * 2 - 1, n_cells[1] * 2 - 1))
 
     # compute Manhattan distances from image center (=radius) of each pixel
-    x = np.arange(-n_cells + 1, n_cells)
-    radii = np.abs(x[np.newaxis]) + np.abs(x[:, np.newaxis])
+    y = np.arange(-n_cells[0] + 1, n_cells[0])
+    x = np.arange(-n_cells[1] + 1, n_cells[1])
+    radii = np.abs(x[np.newaxis]) + np.abs(y[:, np.newaxis])
 
     # add targets
     idx = radii <= (target_radius * 2)
@@ -56,20 +77,98 @@ def dungeon_illusion(
 
     # compute and apply grid mask
     grid_mask = [
-        [(False if i % 2 == 0 and j % 2 == 0 else True) for i in range(n_cells * 2 - 1)]
-        for j in range(n_cells * 2 - 1)
+        [(False if i % 2 == 0 and j % 2 == 0 else True) for i in range(n_cells[1] * 2 - 1)]
+        for j in range(n_cells[0] * 2 - 1)
     ]
     grid_mask = np.array(grid_mask)
     arr[grid_mask] = intensity_background
     mask_arr[grid_mask] = 0
 
-    img = arr.repeat(cell_size_px, axis=0).repeat(cell_size_px, axis=1)
-    mask = mask_arr.repeat(cell_size_px, axis=0).repeat(cell_size_px, axis=1)
-    return {"img": img, "mask": mask}
+    img = arr.repeat(cheight, axis=0).repeat(cwidth, axis=1)
+    mask = mask_arr.repeat(cheight, axis=0).repeat(cwidth, axis=1)
+    
+    # Make sure that stimulus size is as requested
+    if (img.shape[0] != height) or (img.shape[1] != width):
+        img = pad_to_shape(img, (height, width), intensity_background)
+        mask = pad_to_shape(mask, (height, width), 0)
+    
+    stim = {
+        "img": img,
+        "mask": mask.astype(int),
+        "target_radius": target_radius,
+        "intensity_background": intensity_background,
+        "intensity_grid": intensity_grid,
+        "intensity_target": intensity_target,
+        **params,
+        }
+    return stim
+
+
+def resolve_dungeon_params(
+    shape=None,
+    visual_size=None,
+    ppd=None,
+    n_cells=None,
+    cell_size=None,
+):
+
+    # Try to resolve resolution
+    try:
+        shape, visual_size, ppd = resolution.resolve(shape=shape, visual_size=visual_size, ppd=ppd)
+    except ValueError:
+        ppd = resolution.validate_ppd(ppd)
+        shape = resolution.validate_shape(shape)
+        visual_size = resolution.validate_visual_size(visual_size)
+
+    n_cells1, visual_angle1, cell_size1 = resolve_cells_1d(visual_size[0],
+                                                           n_cells[0]-0.5,
+                                                           cell_size[0])
+    n_cells2, visual_angle2, cell_size2 = resolve_cells_1d(visual_size[1],
+                                                           n_cells[1]-0.5,
+                                                           cell_size[1])
+
+    
+    # Now resolve resolution
+    shape, visual_size, ppd = resolution.resolve(
+        shape=shape, visual_size=(visual_angle1/2, visual_angle2/2), ppd=ppd
+    )
+
+    return {
+        "shape": shape,
+        "visual_size": visual_size,
+        "ppd": ppd,
+        "n_cells": (n_cells1+1, n_cells2+1),
+        "cell_size": (cell_size1/2, cell_size2/2),
+    }
+
+
+def resolve_cells_1d(
+    visual_angle=None,
+    n_cells=None,
+    cell_size=None,
+):
+
+    # Try to resolve number and size of cells
+    if cell_size is not None:
+        cells_pd = 1 / cell_size / 2
+    else:
+        cells_pd = None
+
+    try:
+        n_cells, visual_angle, cells_pd = resolution.resolve_1D(
+            length=n_cells, visual_angle=visual_angle, ppd=cells_pd
+        )
+        visual_angle = visual_angle * 2
+        cell_size = 1 / cells_pd
+    except Exception as e:
+        raise Exception("Could not resolve n_cells, cell_size") from e
+
+    return n_cells, visual_angle, cell_size
+
 
 
 if __name__ == "__main__":
     from stimuli.utils import plot_stim
 
-    stim = dungeon_illusion()
+    stim = dungeon_illusion(visual_size=10, ppd=10, n_cells=5)
     plot_stim(stim, stim_name="Dungeon illusion", mask=True, save=None)
