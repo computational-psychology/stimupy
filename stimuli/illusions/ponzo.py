c4ec78a40e9e1b8e341fef3ecba6c10fc6fb4928
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2023-01-13 12:09:13 +0100

    added documentation to ponzo

--- stimuli/illusions/ponzo.py
+++ stimuli/illusions/ponzo.py
@@ -16,7 +16,8 @@ def ponzo_illusion(
     target_lines_length=None,
     target_lines_width=0,
     target_seperation=None,
-    intensity_lines=1.0,
+    intensity_outer_lines=1.0,
+    intensity_target_lines=0.5,
     intensity_background=0.0,
 ):
     """
@@ -30,12 +31,26 @@ def ponzo_illusion(
         pixels per degree [vertical, horizontal]
     shape : Sequence[Number, Number], Number, or None (default)
         shape [height, width] of grating, in pixels
-    element_size : (float, float, float)
-        height, width and thickness of individual elements in degree visual angle
-    intensity_background : float
-        value of background
-    intensity_grid : float
-        value of grid
+    outer_lines_length : Number
+        length of outer lines in degrees visual angle
+    outer_lines_width : Number
+        line width of outer lines in degrees visual angle
+        if 0 (default), set line width to 1 px
+    outer_lines_angle : Number
+        angle of outer lines in degrees. Must be between -45 and 45 degrees.
+    target_lines_length : Number
+        length of target lines in degrees visual angle
+    target_lines_width : 
+        line width of target lines in degrees visual angle
+        if 0 (default), set line width to 1 px
+    target_seperation : Number
+        distance between target lines in degrees visual angle
+    intensity_outer_lines : Number or (Number, Number)
+        intensity value(s) of outer lines
+    intensity_target_lines : Number or (Number, Number)
+        intensity value(s) of target lines
+    intensity_background : Number
+        intensity value of background
 
     Returns
     -------
@@ -43,14 +58,19 @@ def ponzo_illusion(
     
     References
     ----------
-    Hermann L (1870). Eine Erscheinung simultanen Contrastes". Pflügers Archiv
-        fuer die gesamte Physiologie. 3: 13–15. https://doi.org/10.1007/BF01855743
+    Ponzo, M. (1910). Intorno ad alcune illusioni nel campo delle sensazioni
+        tattili, sull'illusione di Aristotele e fenomeni analoghi. Wilhelm
+        Engelmann.
     """
     # Resolve resolution
     shape, visual_size, ppd = resolution.resolve(shape=shape, visual_size=visual_size, ppd=ppd)
     
     if outer_lines_angle < -45 or outer_lines_angle > 45:
         raise ValueError("outer_lines_angle should be between -45 and 45 deg")
+    if isinstance(intensity_outer_lines, (float, int)):
+        intensity_outer_lines = (intensity_outer_lines, intensity_outer_lines)
+    if isinstance(intensity_target_lines, (float, int)):
+        intensity_target_lines = (intensity_target_lines, intensity_target_lines)
     
     line1 = lines.line(
         ppd=ppd,
@@ -59,7 +79,7 @@ def ponzo_illusion(
         line_length=outer_lines_length,
         line_width=outer_lines_width,
         rotation=-outer_lines_angle,
-        intensity_line=intensity_lines-intensity_background,
+        intensity_line=intensity_outer_lines[0]-intensity_background,
         intensity_background=0,
         origin="center",
         )
@@ -71,7 +91,7 @@ def ponzo_illusion(
         line_length=outer_lines_length,
         line_width=outer_lines_width,
         rotation=outer_lines_angle,
-        intensity_line=intensity_lines-intensity_background,
+        intensity_line=intensity_outer_lines[1]-intensity_background,
         intensity_background=0,
         origin="center",
         )
@@ -87,7 +107,7 @@ def ponzo_illusion(
         line_length=target_lines_length,
         line_width=target_lines_width,
         rotation=90,
-        intensity_line=intensity_lines-intensity_background,
+        intensity_line=intensity_target_lines[0]-intensity_background,
         intensity_background=0,
         origin="center",
         )
@@ -99,13 +119,13 @@ def ponzo_illusion(
         line_length=target_lines_length,
         line_width=target_lines_width,
         rotation=90,
-        intensity_line=intensity_lines-intensity_background,
+        intensity_line=intensity_target_lines[1]-intensity_background,
         intensity_background=0,
         origin="center",
         )
     
     line1 = stack_dicts(line1, line2)
-    line1["img"] = line1["img"] + line3["img"] + line4["img"]
+    line1["img"] = line1["img"] + line3["img"] + line4["img"] + intensity_background
     line1["lines_mask"] = line1["mask"] + line3["mask"]*3 + line4["mask"]*4
     line1["mask"] = line3["mask"] + line4["mask"]*2
     return line1
@@ -124,4 +144,4 @@ if __name__ == "__main__":
         }
 
     stim = ponzo_illusion(**p1)
-    plot_stim(stim, stim_name="Ponzo", mask=True, save=None)
+    plot_stim(stim, stim_name="Ponzo", mask=False, save=None)
