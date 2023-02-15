223777a22ebc09260d8b1897f2152945793bc3de
Joris Vincent <joris.vincent@gmail.com>
2023-02-15 08:42:23 +0100

    specify which `mask` to plot
    Closes #109

--- stimuli/utils/plotting.py
+++ stimuli/utils/plotting.py
@@ -1,7 +1,8 @@
+import warnings
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-import warnings
 
 
 def compare_plots(plots):
@@ -13,14 +14,15 @@ def compare_plots(plots):
     plt.show()
 
 
-def plot_stim(stim,
-              mask=False,
-              stim_name="stim",
-              ax=None,
-              vmin=0,
-              vmax=1,
-              save=None,
-              ):
+def plot_stim(
+    stim,
+    mask=False,
+    stim_name="stim",
+    ax=None,
+    vmin=0,
+    vmax=1,
+    save=None,
+):
 
     single_plot = False
     if ax is None:
@@ -31,14 +33,17 @@ def plot_stim(stim,
         ax.imshow(stim["img"], cmap="gray", vmin=vmin, vmax=vmax)
     else:
         img = stim["img"]
-        mask_keys = [key for key in stim.keys() if key.endswith("mask")]
-        
+        if isinstance(mask, str):
+            mask_keys = [mask]
+        else:
+            mask_keys = [key for key in stim.keys() if key.endswith("mask")]
+
         # If target_mask exists, use it.
         if "target_mask" in mask_keys:
             mask = stim["target_mask"]
         else:
             mask = stim[mask_keys[0]]
-        
+
         if (mask is None) or (len(np.unique(mask)) == 1):
             warnings.warn("mask is None or empty- cannot plot")
             ax.imshow(stim["img"], cmap="gray", vmin=vmin, vmax=vmax)
@@ -46,18 +51,18 @@ def plot_stim(stim,
         else:
             img = np.dstack([img, img, img])
             mask = np.dstack([mask, mask, mask])
-    
+
             if np.unique(mask).size > 10:
                 colormap = plt.cm.tab20
             else:
                 colormap = plt.cm.tab10
-    
+
             for idx in np.unique(mask)[np.unique(mask) > 0]:
-                color = colormap.colors[idx%10]
+                color = colormap.colors[idx % 10]
                 color = np.reshape(color, (1, 1, 3))
                 img = np.where(mask == idx, color, img)
             ax.imshow(img)
-    
+
             # Colorbar for mask indices
             bounds = list(np.unique(mask))
             norm = mpl.colors.BoundaryNorm(bounds, len(bounds) + 1, extend="both")
@@ -67,7 +72,7 @@ def plot_stim(stim,
             )
 
     ax.set_title(label=stim_name)
-    
+
     if save is None and single_plot:
         plt.show()
         return ax
@@ -80,26 +85,28 @@ def plot_stim(stim,
         raise ValueError("save can be None or a filepath")
 
 
-def plot_stimuli(stims,
-                 mask=False,
-                 vmin=0,
-                 vmax=1,
-                 save=None,
-                 ):
+def plot_stimuli(
+    stims,
+    mask=False,
+    vmin=0,
+    vmax=1,
+    save=None,
+):
 
     # Plot each stimulus+mask
     n_stim = int(np.ceil(np.sqrt(len(stims))))
     F = plt.figure(figsize=(n_stim * 3, n_stim * 3))
     for i, (stim_name, stim) in enumerate(stims.items()):
         ax = F.add_subplot(n_stim, n_stim, i + 1)
-        plot_stim(stim,
-                  mask,
-                  stim_name=stim_name,
-                  ax=ax,
-                  vmin=vmin,
-                  vmax=vmax,
-                  save=None,
-                  )
+        plot_stim(
+            stim,
+            mask,
+            stim_name=stim_name,
+            ax=ax,
+            vmin=vmin,
+            vmax=vmax,
+            save=None,
+        )
 
     plt.tight_layout()
 
