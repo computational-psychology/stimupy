cd3faf4b6458ac06e3ae1166387af6fbf67c488d
Joris Vincent <joris.vincent@gmail.com>
2023-02-20 10:22:08 +0100

    Test overviews

---
+++ tests/test_overviews.py
@@ -0,0 +1,13 @@
+from stimuli import components, illusions, noises
+
+
+def test_components():
+    components.create_overview()
+
+
+def test_noises():
+    noises.create_overview()
+
+
+def test_illusions():
+    illusions.create_overview()
