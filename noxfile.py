acc007e21ee4ed9350ca161b220216815b0573a8
Joris Vincent <joris.vincent@gmail.com>
2023-03-13 08:14:03 +0100

    Test for Python 3.8 - 3.11

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python="3.11")
+@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def tests(session):
     session.install("-r", "requirements.txt")
     session.install(".")
