27341ecce2afd9a18f9da515a5f0bb325d52472c
lynnschmittwilken <l.schmittwilken@tu-berlin.de>
2023-03-13 18:54:33 -0400

    merge conflicts part 2

--- tests/papers/test_modelfest.py
+++ tests/papers/test_modelfest.py
@@ -8,13 +8,8 @@ from stimupy.papers.modelfest import __all__ as stimlist
 from stimupy.utils import export
 
 data_dir = os.path.dirname(__file__)
-<<<<<<< HEAD:tests/papers/test_modelfest.py
 jsonfile = os.path.join(data_dir, "modelfest.json")
-loaded = json.load(open(jsonfile, "r"))
-=======
-jsonfile = os.path.join(data_dir, "carney1999.json")
 loaded = json.load(open(jsonfile))
->>>>>>> a5203fe9c6aa03c15510f888960e120351f720f8:tests/papers/test_carney1999.py
 
 
 @pytest.mark.parametrize("stim_name", stimlist)
