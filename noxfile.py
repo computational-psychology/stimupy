1b711f685ab56045f40d84f95ac2b26b7cae3de1
Joris Vincent <joris.vincent@gmail.com>
2023-03-13 11:20:48 +0100

    Include pre-commit hook in Nox

--- noxfile.py
+++ noxfile.py
@@ -6,3 +6,10 @@ def tests(session):
     session.install("-r", "requirements.txt")
     session.install(".")
     session.run("pytest")
+
+
+@nox.session(python="3.11")
+def lint(session):
+    # Run the linters (via pre-commit)
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
