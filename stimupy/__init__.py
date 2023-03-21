3428970037398bd24fc1331611b0c4edcc61d298
Joris Vincent <joris.vincent@gmail.com>
2023-03-21 10:17:45 +0100

    Create super-overview

--- stimupy/__init__.py
+++ stimupy/__init__.py
@@ -20,3 +20,49 @@ from . import (
     wedding_cakes,
     whites,
 )
+
+__stimuli__ = [
+    "benarys",
+    "cubes",
+    "delboeufs",
+    "dungeons",
+    "gabors",
+    "hermanns",
+    "mondrians",
+    "mueller_lyers",
+    "plaids",
+    "ponzos",
+    "sbcs",
+    "todorovics",
+    "wedding_cakes",
+    "whites",
+]
+
+
+def overview(skip=False):
+    """Generate example stimuli from this module
+
+    Returns
+    -------
+    dict[str, dict]
+        Dict mapping names to individual stimulus dicts
+    """
+
+    stimuli = {}
+    for stimmodule_name in __stimuli__:
+        print(f"Generating stimuli from {stimmodule_name}")
+        # Get a reference to the actual module
+        stimmodule = globals()[stimmodule_name]
+        try:
+            stims = stimmodule.overview()
+
+            # Accumulate
+            stimuli.update(stims)
+        except NotImplementedError as e:
+            if not skip:
+                raise e
+            # Skip stimuli that aren't implemented
+            print("-- not implemented")
+            pass
+
+    return stimuli
