1fec019c204a40a3f46b082bb0c74aee3193e4b9
Joris Vincent <joris.vincent@gmail.com>
2023-03-21 14:59:25 +0100

    Add Cityblock waves

--- stimupy/waves.py
+++ stimupy/waves.py
@@ -11,8 +11,8 @@ __all__ = [
     # "staircase_linear",
     "sine_radial",
     "square_radial",
-    # "sine_cityblock",
-    # "square_cityblock",
+    "sine_cityblock",
+    "square_cityblock",
     # "sine_angular",
     # "square_angular",
 ]
@@ -492,6 +492,210 @@ def square_radial(
     return stim
 
 
+def sine_cityblock(
+    visual_size=None,
+    ppd=None,
+    shape=None,
+    frequency=None,
+    n_frames=None,
+    frame_width=None,
+    period="ignore",
+    rotation=0,
+    phase_shift=0,
+    intensities=(0.0, 1.0),
+    target_indices=(),
+    intensity_target=0.5,
+    origin="mean",
+    round_phase_width=True,
+):
+    """Rectilinear sine-wave grating (set of frames) over the whole image, with some frame(s) as target(s)
+
+    Parameters
+    ----------
+    visual_size : Sequence[Number, Number], Number, or None (default)
+        visual size [height, width] of image, in degrees
+    ppd : Sequence[Number, Number], Number, or None (default)
+        pixels per degree [vertical, horizontal]
+    shape : Sequence[Number, Number], Number, or None (default)
+        shape [height, width] of image, in pixels
+    frequency : Number, or None (default)
+        spatial frequency of grating, in cycles per degree visual angle
+    n_frames : int, or None (default)
+        number of frames
+    frame_width : Number, or None (default)
+        width of a single frame, in degrees
+    period : "even", "odd", "either" or "ignore" (default)
+        ensure whether the grating has "even" number of phases, "odd"
+        number of phases, either or whether not to round the number of
+        phases ("ignore")
+    rotation : float
+        rotation of grating in degrees (default: 0 = horizontal)
+    phase_shift : float
+        phase shift of grating in degrees
+    intensities : Sequence[float, float] or None (default)
+        min and max intensity of sine-wave
+    target_indices : int, or Sequence[int, ...]
+        indices segments where targets will be placed
+    intensity_target : float, or Sequence[float, ...], optional
+        intensity value for each target, by default 0.5.
+        Can specify as many intensities as number of target_indices;
+        If fewer intensities are passed than target_indices, cycles through intensities
+    origin : "corner", "mean" or "center"
+        if "corner": set origin to upper left corner
+        if "mean": set origin to hypothetical image center (default)
+        if "center": set origin to real center (closest existing value to mean)
+    round_phase_width : Bool
+        if True, round width of frames given resolution
+
+    Returns
+    -------
+    dict[str, Any]
+        dict with the stimulus (key: "img"),
+        mask with integer index for each target (key: "target_mask"),
+        and additional keys containing stimulus parameters
+    """
+    if len(intensities) != 2:
+        raise ValueError("intensity_frames should be [float, float]")
+
+    lst = [visual_size, ppd, shape, frequency, n_frames, frame_width]
+    if len([x for x in lst if x is not None]) < 3:
+        raise ValueError(
+            "'grating' needs 3 non-None arguments for resolving from 'visual_size', "
+            "'ppd', 'shape', 'frequency', 'n_frames', 'frames_width'"
+        )
+
+    # Spatial square-wave grating
+    stim = waves.sine(
+        visual_size=visual_size,
+        ppd=ppd,
+        shape=shape,
+        frequency=frequency,
+        n_phases=n_frames,
+        phase_width=frame_width,
+        period=period,
+        rotation=rotation,
+        phase_shift=phase_shift,
+        intensities=intensities,
+        origin=origin,
+        round_phase_width=round_phase_width,
+        base_type="cityblock",
+    )
+
+    # Repackage output
+    stim["n_frames"] = stim.pop("n_phases")
+    stim["frame_width"] = stim.pop("phase_width")
+    stim.pop("base_type")
+
+    # Add targets(?)
+    if target_indices is not None and target_indices != ():
+        stim = add_targets(stim, target_indices=target_indices, intensity_target=intensity_target)
+
+    return stim
+
+
+def square_cityblock(
+    visual_size=None,
+    ppd=None,
+    shape=None,
+    frequency=None,
+    n_frames=None,
+    frame_width=None,
+    period="ignore",
+    rotation=0,
+    phase_shift=0,
+    intensity_frames=(1.0, 0.0),
+    target_indices=(),
+    intensity_target=0.5,
+    origin="mean",
+    round_phase_width=True,
+):
+    """Rectilinear square-wave grating (set of frames) over the whole image, with some frame(s) as target(s)
+
+    Parameters
+    ----------
+    visual_size : Sequence[Number, Number], Number, or None (default)
+        visual size [height, width] of image, in degrees
+    ppd : Sequence[Number, Number], Number, or None (default)
+        pixels per degree [vertical, horizontal]
+    shape : Sequence[Number, Number], Number, or None (default)
+        shape [height, width] of image, in pixels
+    frequency : Number, or None (default)
+        spatial frequency of grating, in cycles per degree visual angle
+    n_frames : int, or None (default)
+        number of frames in the grating
+    frame_width : Number, or None (default)
+        width of a single frame, in degrees visual angle
+    period : "even", "odd", "either" or "ignore" (default)
+        ensure whether the grating has "even" number of phases, "odd"
+        number of phases, either or whether not to round the number of
+        phases ("ignore")
+    rotation : float
+        rotation of grating in degrees (default: 0 = horizontal)
+    phase_shift : float
+        phase shift of grating in degrees
+    intensity_frames : Sequence[float, ...]
+        intensity value for each frame, by default (1.0, 0.0).
+        Can specify as many intensities as n_frames;
+        If fewer intensities are passed than n_frames, cycles through intensities
+    target_indices : int, or Sequence[int, ...]
+        indices segments where targets will be placed
+    intensity_target : float, or Sequence[float, ...], optional
+        intensity value for each target, by default 0.5.
+        Can specify as many intensities as number of target_indices;
+        If fewer intensities are passed than target_indices, cycles through intensities
+    origin : "corner", "mean" or "center"
+        if "corner": set origin to upper left corner
+        if "mean": set origin to hypothetical image center (default)
+        if "center": set origin to real center (closest existing value to mean)
+    round_phase_width : Bool
+        if True, round width of frames given resolution
+
+    Returns
+    -------
+    dict[str, Any]
+        dict with the stimulus (key: "img"),
+        mask with integer index for each target (key: "target_mask"),
+        and additional keys containing stimulus parameters
+    """
+    lst = [visual_size, ppd, shape, frequency, n_frames, frame_width]
+    if len([x for x in lst if x is not None]) < 3:
+        raise ValueError(
+            "'grating' needs 3 non-None arguments for resolving from 'visual_size', "
+            "'ppd', 'shape', 'frequency', 'n_frames', 'frame_width'"
+        )
+
+    # Spatial square-wave grating
+    stim = waves.square(
+        visual_size=visual_size,
+        ppd=ppd,
+        shape=shape,
+        frequency=frequency,
+        n_phases=n_frames,
+        phase_width=frame_width,
+        period=period,
+        rotation=rotation,
+        phase_shift=phase_shift,
+        origin=origin,
+        round_phase_width=round_phase_width,
+        base_type="cityblock",
+    )
+
+    # Adjust intensities to passed-in values
+    stim["img"] = draw_regions(mask=stim["grating_mask"], intensities=intensity_frames)
+
+    # Repackage output
+    stim["n_frames"] = stim.pop("n_phases")
+    stim["frame_width"] = stim.pop("phase_width")
+    stim["intensity_frames"] = stim.pop("intensities")
+    stim.pop("base_type")
+
+    # Add targets(?)
+    if target_indices is not None and target_indices != ():
+        stim = add_targets(stim, target_indices=target_indices, intensity_target=intensity_target)
+
+    return stim
+
+
 def overview(**kwargs):
     """Generate example stimuli from this module
 
@@ -516,17 +720,18 @@ def overview(**kwargs):
         "sine wave - vertical": sine_linear(**default_params, **grating_params, bar_width=1, rotation=90),
         "sine wave - oblique": sine_linear(**default_params, **grating_params, bar_width=1, rotation=45),
         "sine wave - radial": sine_radial(**default_params, **grating_params, ring_width=1, clip=True),
+        "sine wave - cityblock": sine_cityblock(**default_params, **grating_params, frame_width=1),
+
 
         "square wave - horizontal": square_linear(**default_params, **grating_params, bar_width=1, rotation=0),
         "square wave - vertical": square_linear(**default_params, **grating_params, bar_width=1, rotation=90),
         "square wave - oblique": square_linear(**default_params, **grating_params, bar_width=1, rotation=45),
         "square wave - radial": square_radial(**default_params, **grating_params, ring_width=1, clip=True),
+        "square wave - cityblock": square_cityblock(**default_params, **grating_params, frame_width=1),
 
         # "sine wave - angular": sine(**default_params, **grating_params, base_type="angular"),
-        # "sine wave - cityblock": sine(**default_params, **grating_params, base_type="cityblock"),
 
         # "square wave - angular": square(**default_params, **grating_params, base_type="angular"),
-        # "square wave - cityblock": square(**default_params, **grating_params, base_type="cityblock"),
 
         "square wave - multiple intensitites": square_linear(**default_params, **grating_params, bar_width=3.5, rotation=0, intensity_bars=(0, 0.25, 0.75)),
     }
